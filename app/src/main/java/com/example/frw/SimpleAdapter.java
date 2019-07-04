0fbc9f17e5cca87243475493471986737f0aa095
FaRzad <shamifarzad45@gmail.com>
2019-07-04 15:45:03 +0430

    simpleAdaptor trick to avoid Skip layout in profile activity ... if errors happens the layout goes empty .... and we do not have any error

---
+++ app/src/main/java/com/example/frw/SimpleAdapter.java
@@ -0,0 +1,32 @@
+package com.example.frw;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SimpleAdapter extends RecyclerView.Adapter<SimpleAdapter.SampleHolder> {
+
+    @NonNull
+    @Override
+    public SampleHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SampleHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class SampleHolder extends RecyclerView.ViewHolder {
+        public SampleHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
